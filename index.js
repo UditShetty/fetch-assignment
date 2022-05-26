0fbf0904c4e433c4c5e91b7d365357246f06ed13
Uditshetty <udit123.uv@gmail.com>
2022-05-26 09:28:51 +0530

    adding fetch api

---
+++ index.js
@@ -0,0 +1,50 @@
+// var Apikey="f69b3aa4"
+ 
+ function searchMovie(){
+    let movie=document.getElementById("movie").value
+      console.log(movie)
+
+      let url=`http://www.omdbapi.com/?t=${movie}&apikey=d5c826c7`
+
+      async function getData(){
+        try{
+            let res= await fetch(url)
+    
+            //  console.log(res)
+        
+            let users= await res.json()
+        
+            // append(users)
+            displayMovies(users)
+            console.log(users)
+        }catch(err){
+            console.log(err)
+        }
+     }
+    
+     getData()
+ }
+
+ function displayMovies(movie){
+      let div = document.createElement("div")
+
+      let image=document.createElement("img")
+      image.src=movie.Poster
+
+      let title=document.createElement("p")
+      title.innerHTML="Title:"+ movie.Title
+
+      let year= document.createElement("p")
+      year.innerText="Year:"+ movie.Year
+
+      let released= document.createElement("p")
+      released.innerText="Released:"+ movie.Released
+
+
+      div.append(image,title,year,released)
+      document.querySelector("#container").append(div)
+
+
+ }
+ 
+
